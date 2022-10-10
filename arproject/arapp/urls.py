b96f731ffe36aa1c0ffe5639f64f3e328a61eb61
KeitoH <ht20a082@oecu.jp>
2022-10-10 18:28:39 +0900

    create ar_cam

--- arproject/arapp/urls.py
+++ arproject/arapp/urls.py
@@ -1,1 +1,10 @@
-urlpatterns = []
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('ar_cam/', views.Ar_camViews.as_view(), name="ar_cam"),
+    path('video_feed/', views.video_feed_view(), name="video_feed"),
+]
--- arproject/arapp/views.py
+++ arproject/arapp/views.py
@@ -1,3 +1,31 @@
+import cv2
+from django.http import StreamingHttpResponse
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class Ar_camViews(View):
+    def get(self, request):
+        return render(request, 'arapp/ar_cam.html', {})
+
+def video_feed_view():
+    return lambda _: StreamingHttpResponse(generate_frame(), content_type='multipart/x-mixed-replace; boundary=frame')
+
+def generate_frame():
+    capture = cv2.VideoCapture(0) 
+
+    while True:
+        if not capture.isOpened():
+            print("Capture is not opened.")
+            break
+        ret, frame = capture.read()
+        if not ret:
+            print("Failed to read frame.")
+            break
+
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        byte_frame = jpeg.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + byte_frame + b'\r\n\r\n')
+    capture.release()
