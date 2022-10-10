3b4aa50775d4fcf0a1261a6669b8ef1b3071c7ef
KeitoH <ht20a082@oecu.jp>
2022-10-10 15:57:38 +0900

    create template

--- arproject/mainapp/urls.py
+++ arproject/mainapp/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('synthesized/', views.ListSynthesizedView.as_View()),
-    path('image/', views.ListImageView.as_View()),
+    path('synthesized/', views.ListSynthesizedView.as_View(), name='synthesized-main'),
+    path('image/', views.ListImageView.as_View(), name='image-main'),
 ]
