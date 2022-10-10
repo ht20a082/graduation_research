8a85574c4fcc1f94a4703a6fcd394b2b57ffdf38
KeitoH <ht20a082@oecu.jp>
2022-10-10 16:56:32 +0900

    create top

--- arproject/mainapp/urls.py
+++ arproject/mainapp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('synthesized/', views.ListSynthesizedView.as_View(), name='synthesized-main'),
-    path('image/', views.ListImageView.as_View(), name='image-main'),
+    path('', views.Top_view.as_view(), name='top'),
+    path('synthesized/', views.ListSynthesizedView.as_view(), name='synthesized-main'),
+    path('image/', views.ListImageView.as_view(), name='image-main'),
 ]
--- arproject/mainapp/views.py
+++ arproject/mainapp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import TemplateView, ListView
+from django.views import View
 
 
-class ListSynthesizedView(ListView):
+class Top_view(TemplateView):
+    template_name = 'mainapp/top.html'
+
+class ListSynthesizedView(TemplateView):
     template_name = 'mainapp/synthesized_list.html'
 
-class ListImageView(ListView):
+class ListImageView(TemplateView):
     template_name = 'mainapp/image_list.html'
