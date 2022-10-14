1942be6bd4b04d4150a5303ebc7d1b237a7e063b
KeitoH <ht20a082@oecu.jp>
2022-10-14 12:24:31 +0900

    create model Image

--- arproject/mainapp/models.py
+++ arproject/mainapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    title = models.CharField(max_length=100)
+    height = models.IntegerField()
+    width = models.IntegerField()
+
+    def __str__(self):
+        return self.title
