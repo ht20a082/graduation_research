b7bf1097243731a880f069930f606f71dc594d13
KeitoH <ht20a082@oecu.jp>
2022-10-16 22:38:37 +0900

    requirements.txt update

--- arproject/mainapp/models.py
+++ arproject/mainapp/models.py
@@ -4,6 +4,7 @@ class Image(models.Model):
     title = models.CharField(max_length=100)
     height = models.IntegerField()
     width = models.IntegerField()
+    thumbnail = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.title
