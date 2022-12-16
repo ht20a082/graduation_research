faa3d01cf85a6c0c09088fd10591ee14bdda4fe9
KeitoH <ht20a082@oecu.jp>
2022-12-16 11:23:41 +0900

    fix model

--- arproject/mainapp/models.py
+++ arproject/mainapp/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Image(models.Model):
     title = models.CharField(max_length=100)
-    height = models.IntegerField()
-    width = models.IntegerField()
-    thumbnail = models.ImageField(null=True, blank=True)
+    height = models.IntegerField(validators=[MinValueValidator(1)])
+    width = models.IntegerField(validators=[MinValueValidator(1)])
+    thumbnail = models.ImageField(null=False, blank=False)
 
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
 
