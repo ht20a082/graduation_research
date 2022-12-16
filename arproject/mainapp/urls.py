16b82f4835d7c7455b843de90e09faf2037b69d7
KeitoH <ht20a082@oecu.jp>
2022-12-16 11:32:52 +0900

    delete Synthesized

--- arproject/mainapp/urls.py
+++ arproject/mainapp/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.Top_view.as_view(), name='top'),
-    path('synthesized/', views.ListSynthesizedView.as_view(), name='synthesized-main'),
     path('image/', views.ListImageView.as_view(), name='image-main'),
     path('image/create/', views.CreateImageView.as_view(), name='image-create'),
     path('image/detail/<int:pk>/', views.DetailImageView.as_view(), name='image-detail'),
